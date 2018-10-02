81be54e2225c4ab8ef09221c17d017c4e7f3894d
mrzipapupa <mypokypka@mail.ru>
2018-10-03 01:52:57 +0300

    1st hw

---
+++ lesson1(hard).py
@@ -0,0 +1,11 @@
+__author__ = 'Герасимов Никита Алексеевич'
+
+if __name__ == "__main__":
+    number = float('inf')
+    if number == number ** 2:
+        print('true')
+    if number == number * 2:
+        print('true')
+    if number > 99999:
+        print('true')
+
